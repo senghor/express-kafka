6df434227df586cd59c27861133d1e41c4c541d5
Albert Senghor <asenghor@gmail.com>
2024-05-19 11:36:52 +0300

    Added publishers and consumers

--- kafkaController.js
+++ kafkaController.js
@@ -16,24 +16,70 @@ class KafkaController {
  * Create topic with multiple partitions
  */
     async createTopic (topicName, numPartitions) {
-    try {
-        //Create admin client to manage kafka topic
-        const admin = this.kafka.admin()
-        await admin.connect()
-        await admin.createTopics({
-            topics: [{
-                topic: topicName.toString(), 
-                numPartitions: parseInt(numPartitions),
-                replicationFactor: 1
-            }]
-        })
-        await admin.disconnect()
+        try {
+            //Create admin client to manage kafka topic
+            const admin = this.kafka.admin()
+            await admin.connect()
+            await admin.createTopics({
+                topics: [{
+                    topic: topicName.toString(), 
+                    numPartitions: parseInt(numPartitions),
+                    replicationFactor: 1
+                }]
+            })
+            await admin.disconnect()
 
-    } catch(e) {
-        console.log(e)
-        throw e
+        } catch(e) {
+            console.log(e)
+            throw e
+            }
+        }
+    /**
+     * publish message to the topic
+     */
+    async publishMessage(topicName, messages) {
+        const producer = this.kafka.producer()
+        try {
+            await producer.connect()
+            await producer.send({
+                topic: topicName,
+                messages
+            })
+        } catch(e) {
+            console.log(e)
+            throw e
+        } finally {
+            producer.disconnect()
+        }
     }
-}
+
+    /**
+     * Consume message from the topic
+     */
+    async consumeMessage(topicName, callback) {
+       const consumer = this.kafka.consumer({groupId: 'my-group'}) 
+       try {
+        await consumer.connect()
+        await consumer.subscribe({
+            topic: topicName,
+            fromBeginning: true
+        })
+        await consumer.run({
+            eachMessage: async ({
+                topic,
+                partition,
+                message
+            }) => {
+                const value = `Received message: ${message.value.toLocaleString()} 
+                from partition ${partition} $ topic ${topic} `;
+                callback(value)
+            }
+        })
+       } catch(e) {
+            console.log(e)
+            throw e
+       }
+    }
 }
 
 export default KafkaController
--- routes.js
+++ routes.js
@@ -2,10 +2,11 @@ import { Router } from 'express'
 const router = Router()
 
 import KafkaController from './kafkaController.js'
-const kafkaController = new KafkaController()
+
 
 router.post('/create_topic', async (req, res) => {
     try {
+        const kafkaController = new KafkaController()
         const { topicName, numPartitions } = req.body
         await kafkaController.createTopic(topicName, numPartitions)
         res.send({
@@ -16,6 +17,48 @@ router.post('/create_topic', async (req, res) => {
         console.log(e)
         res.status(500).send({
             message: 'Failed to create topic'
+        })
+    }
+})
+
+router.post('/publish', async (req, res) => {
+    try {
+        const {topicName, message} = req.body
+        const messages = [
+            {
+                key: message?.key,
+                value: message?.value
+            }
+        ]
+        const kafkaController = new KafkaController()
+        await kafkaController.publishMessage(topicName, messages)
+        res.send({
+            status: 'OK',
+            message: 'Message successifully published!'
+        })
+    } catch(e) {
+        console.log(e)
+        res.status(500).send({
+            message: 'Failed to publish message to the topic'
+        })
+    }
+})
+
+router.post('/consume', async (req, res) => {
+    try {
+        const {topicName} = req.body
+        const kafkaController = new KafkaController()
+        await kafkaController.consumeMessage(topicName, message => {
+            res.send({
+                status: 'OK',
+                message
+            })
+        })
+       
+    } catch(e) {
+        console.log(e)
+        res.status(500).send({
+            message: 'Failed to consume message from the topic'
         })
     }
 })
